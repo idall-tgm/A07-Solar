b82adf1e4e06f4ead0eefb51dc370347b095e950
Bernhard Schwertberger <schwertberger.bernhard@gmail.com>
2015-03-19 19:07:36 +0100

    Zooooooooooooom
    Zweite Spheren möglichkeit + Textur
    Funktionierende Zoom Funktion

--- Solar.py
+++ Solar.py
@@ -14,42 +14,7 @@ import sys
 import pyglet.image
 from pyglet.gl import *
 
-def Cube():
-    verticies = (
-    (1, -1, -1),
-    (1, 1, -1),
-    (-1, 1, -1),
-    (-1, -1, -1),
-    (1, -1, 1),
-    (1, 1, 1),
-    (-1, -1, 1),
-    (-1, 1, 1),
-    )
-
-    edges = (
-        (0, 1),
-        (0, 3),
-        (0, 4),
-        (2, 1),
-        (2, 3),
-        (2, 7),
-        (6, 3),
-        (6, 4),
-        (6, 7),
-        (5, 1),
-        (5, 4),
-        (5, 7),
-    )
-
-    glBegin(GL_LINES)
-    for edge in edges:
-        for vertex in edge:
-            glVertex3fv(verticies[vertex])
-    glEnd()
-
-
-def Sphere1(radius):
-    glEnable(GL_TEXTURE_2D)
+def Sphere1(radius, sphere):
     image=pyglet.image.load("sonne.png")
     texture = image.get_texture()
     glEnable(texture.target)
@@ -62,64 +27,26 @@ def Sphere1(radius):
     image = rawimage.get_data(format,pitch)
     '''
 
-    sphere = gluNewQuadric()
+
     #glColor4f(1,1,0.2,1)
-    gluSphere(sphere, radius, 100, 100)
+    gluSphere(sphere, radius, 20, 20)
     glDisable(GL_TEXTURE_2D)
 
 
+def Sphere2(radius, sphere):
 
 
 
-def load_Texture():
-
-    #global texture
-    glEnable(GL_TEXTURE_2D)
-    image = pyglet.image.load('sonne.png')
-
-
-   # ix = image.width
-   # iy = image.height
+    '''
     rawimage = image.get_image_data()
     format = 'RGBA'
     pitch = rawimage.width * len(format)
     image = rawimage.get_data(format,pitch)
-
-    texture = image.get_texture()
-    glEnable(texture.target)
-    glBindTexture(texture.target, texture.id)
-
     '''
-    #Create Texture
-    textures = glGenTextures(3)
-    glBindTexture(GL_TEXTURE_2D, int(textures[0]))   # 2d texture (x and y size)
 
-
-    # Create Linear Filtered Texture
-    glBindTexture(GL_TEXTURE_2D, int(textures[1]))
-    glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MAG_FILTER,GL_LINEAR)
-    glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MIN_FILTER,GL_LINEAR)
-    glTexImage2D(GL_TEXTURE_2D, 0, 3, ix, iy, 0, GL_RGBA, GL_UNSIGNED_BYTE, image)
-
-
-    # Create MipMapped Texture
-    glBindTexture(GL_TEXTURE_2D, int(textures[2]))
-    glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MAG_FILTER,GL_LINEAR)
-    glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MIN_FILTER,GL_LINEAR_MIPMAP_NEAREST)
-    gluBuild2DMipmaps(GL_TEXTURE_2D, 3,ix,iy, GL_RGBA, GL_UNSIGNED_BYTE, image)
-'''
-    sphere = gluNewQuadric()
-    gluQuadricNormals(sphere,GLU_SMOOTH)
-    gluQuadricTexture(sphere,GL_TRUE)
-
-    '''
-    glEnable(GL_TEXTURE_2D)
-    glClearColor(0.0, 0.0, 0.0, 0.0)	# This Will Clear The Background Color To Black
-    glClearDepth(1.0)					# Enables Clearing Of The Depth Buffer
-    glDepthFunc(GL_LESS)				# The Type Of Depth Test To Do
-    glEnable(GL_DEPTH_TEST)				# Enables Depth Testing
-    glShadeModel(GL_SMOOTH)				# Enables Smooth Color Shading
-    '''
+    #glColor4f(1,1,0.2,1)
+    gluSphere(sphere, radius, 20, 20)
+    glDisable(GL_TEXTURE_2D)
 
 def setupLighting():
     """ Initializing Lighting and Light0
@@ -156,6 +83,7 @@ def main():
     speed = 1
     modestatus = 1
     yrot = speed
+    distanceset = 0
 
     pygame.init()
 
@@ -194,10 +122,13 @@ def main():
 
             #setupLighting()
 
-            gluPerspective(fov, (size[0] / size[1]), 0.1, 50.0)
+            gluPerspective(fov, (size[0] / size[1]), 0.1, 100.0)
 
-            glTranslatef(0.0, 0.0, -5)
+            glEnable(GL_TEXTURE_2D)
 
+            sphere = gluNewQuadric()
+            gluQuadricNormals(sphere,GLU_SMOOTH)
+            gluQuadricTexture(sphere,GL_TRUE)
 
             modestatus = 3
 
@@ -206,66 +137,70 @@ def main():
 
         elif modestatus == 3:
 
+            if distanceset == 0:
+                glTranslatef(0.0, 0.0, -7)
+                distanceset = 1
+
             glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
 
             glPushMatrix()
 
             glRotatef(yrot,0,1,0)
 
-            Sphere1(1)
-            #Cube()
+            Sphere1(1,sphere)
+
             glPopMatrix()
 
             glPushMatrix()
-            glRotatef(4.5*yrot,0,1,0.2)
+            glRotatef(4.5*yrot,0,1,0.3)
             glTranslatef(2,0,0)
-            Sphere1(0.035)
+            Sphere2(0.035,sphere)
             glPopMatrix()
 
             glPushMatrix()
-            glRotatef(1.6*yrot,0,1,0.2)
+            glRotatef(1.6*yrot,0,1,0.3)
             glTranslatef(3,0,0)
-            Sphere1(0.086)
+            Sphere2(0.086,sphere)
             glPopMatrix()
 
             glPushMatrix()
-            glRotatef(yrot,0,1,0.2)
+            glRotatef(yrot,0,1,0.3)
             glTranslatef(4,0,0)
-            Sphere1(0.091)
+            Sphere2(0.091,sphere)
 
-            glTranslatef(0.1,0,0)
-            glRotatef(12*yrot,0,1,0.2)
-            Sphere1(0.025)
+            glTranslatef(0.2,0,0)
+            glRotatef(12*yrot,0,1,0.3)
+            Sphere2(0.025,sphere)
             glPopMatrix()
 
             glPushMatrix()
-            glRotatef(yrot/2,0,1,0.2)
+            glRotatef(yrot/2,0,1,0.3)
             glTranslatef(5,0,0)
-            Sphere1(0.049)
+            Sphere2(0.049,sphere)
             glPopMatrix()
 
             glPushMatrix()
-            glRotatef(yrot/12,0,1,0.2)
+            glRotatef(yrot/12,0,1,0.3)
             glTranslatef(7,0,0)
-            Sphere1(0.102)
+            Sphere2(0.102,sphere)
             glPopMatrix()
 
             glPushMatrix()
-            glRotatef(yrot/30,0,1,0.2)
+            glRotatef(yrot/30,0,1,0.3)
             glTranslatef(9,0,0)
-            Sphere1(0.086)
+            Sphere2(0.086,sphere)
             glPopMatrix()
 
             glPushMatrix()
-            glRotatef(yrot/84,0,1,0.2)
+            glRotatef(yrot/84,0,1,0.3)
             glTranslatef(11,0,0)
-            Sphere1(0.037)
+            Sphere2(0.037,sphere)
             glPopMatrix()
 
             glPushMatrix()
-            glRotatef(yrot/164,0,1,0)
+            glRotatef(yrot/164,0,1,0.3)
             glTranslatef(12,0,0)
-            Sphere1(0.035)
+            Sphere2(0.035,sphere)
             glPopMatrix()
 
             yrot += speed
@@ -292,9 +227,16 @@ def main():
                 elif event.type == MOUSEBUTTONDOWN:
                     if event.button == 4 and fov > 50:
                         fov -= 5
+                        glLoadIdentity()
+
+                        gluPerspective(fov, (size[0] / size[1]), 0.1, 50.0)
+                        distanceset = 0
 
                     elif event.button == 5 and fov < 150:
                         fov += 5
+                        glLoadIdentity()
+                        gluPerspective(fov, (size[0] / size[1]), 0.1, 50.0)
+                        distanceset = 0
 
 
         elif modestatus == 4:
